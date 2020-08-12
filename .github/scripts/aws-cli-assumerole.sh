be4683880a670b5f86b45f676e9d9525460e6629
youjinp <youjinp@users.noreply.github.com>
2020-08-13 09:39:35 +1200

    Fix assume role script

--- .github/scripts/aws-cli-assumerole.sh
+++ .github/scripts/aws-cli-assumerole.sh
@@ -2,14 +2,11 @@ unset  AWS_SESSION_TOKEN
 
 temp_role=$(aws sts assume-role \
                     --role-arn "${TEST_ROLE}" \
-                    --role-session-name "vgaltes-prod" \
-                    --profile vgaltes-serverless)
+                    --role-session-name "github-action-test"
 
 AWS_ACCESS_KEY_ID=$(echo $temp_role | jq .Credentials.AccessKeyId | xargs)
 AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq .Credentials.SecretAccessKey | xargs)
 AWS_SESSION_TOKEN=$(echo $temp_role | jq .Credentials.SessionToken | xargs)
-
-echo "::set-env name=VERSION::19.2.5"
 
 echo "::set-env name=AWS_ACCESS_KEY_ID::${AWS_ACCESS_KEY_ID}"
 echo "::set-env name=AWS_SECRET_ACCESS_KEY::${AWS_SECRET_ACCESS_KEY}"
